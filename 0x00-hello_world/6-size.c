54c2e4f0b3849ad699be5ff436f30b1813309b4e
sunnexy <sogbonnapaschal@gmail.com>
2022-03-10 14:17:28 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 * main - print the string in the put function
 * Description: using the main function
 * this program prints Programming is like building a multilingual puzzle
-* Return: 0
+* Return: always 0(success)
 */
 int main(void)
 {
