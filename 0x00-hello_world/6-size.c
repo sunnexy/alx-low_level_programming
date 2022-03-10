34eb7f51d47c482c3bda556257ea8142120b3838
sunnexy <sogbonnapaschal@gmail.com>
2022-03-10 12:56:47 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
+ * main print the string in the put function
  * Description:using the main function
  * this program prints different sizes
  * Return 0
  */
-/* main */
 int main(void)
 {
 char c;
