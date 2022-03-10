70f5cbfa351932d01890d7e1bd317147a614ec4a
sunnexy <sogbonnapaschal@gmail.com>
2022-03-10 10:53:51 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 
 /**
  *
- *  *main - prints the phrase with proper grammar,
- *  * prints the statement
- *   *Return: 0 if exited properly, non zero otherwise
- *
- *    */
+ *main - prints the phrase with proper grammar,
+ * prints the statement
+ *Return: 0 if exited properly, non zero otherwise
+ */
 
 int main(void)
 
