c787c36b3ea29aa1c9f3f2d47d6167fc101aa8c4
sunnexy <sogbonnapaschal@gmail.com>
2022-03-10 14:46:34 -0800

    quo

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n")
+	return (1);
+}
