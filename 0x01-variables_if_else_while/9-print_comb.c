72d1c399c4b82d01b25390841ea930856063f65d
sunnexy <sogbonnapaschal@gmail.com>
2022-03-11 13:23:12 -0800

    all

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,9 @@ int main(void)
 	{
 		putchar(n);
 		if (n != 57)
-		{																		putchar(',');																putchar(' ');
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
