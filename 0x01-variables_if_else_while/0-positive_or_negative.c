5de9afcc89638bd9e4b12df40b9b6c3cc552f022
sunnexy <sogbonnapaschal@gmail.com>
2022-03-11 10:12:05 -0800

    pos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /*
-* main - assigns a random number to int n everytime
-*
-* Return: Always 0 (Success)
+*main - entry point
+*Description: tell if a number is postive, zero or negative
+*Return: Always 0 (Success)
 */
 int main(void)
 {
