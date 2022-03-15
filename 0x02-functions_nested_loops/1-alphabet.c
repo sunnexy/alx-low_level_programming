c7cb6f6bf8e7f80432399dc191c2d1f6f4c92d63
sunnexy <sogbonnapaschal@gmail.com>
2022-03-15 14:22:24 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints alphabet
+ * Description: prints alphabeths in small letter
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet("abcdefghijklmnopqrstuvwxyz");
+	_putchar('\n')
+
+	return (0);
+}
