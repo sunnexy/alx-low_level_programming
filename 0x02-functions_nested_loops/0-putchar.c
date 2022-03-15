db1925d418539f7467ce8d9a8875efe3dadefa37
sunnexy <sogbonnapaschal@gmail.com>
2022-03-15 04:05:26 -0700

    put

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: printing puchar
+ * return: always 0
+ */
+int main (void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'A'; a++)
+		putchat(a);
+	putchar('\n');
+	return (0);
+}
