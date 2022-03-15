13cd78b573b11a78b7aa841a1896aac3c8f6b8d6
sunnexy <sogbonnapaschal@gmail.com>
2022-03-15 15:36:33 -0700

    slow

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#iinclude "main.h"
+/**
+ * _islower - checks for lowercase
+ *   * @c: c is an ascii character
+ * Description: checks for lowercase
+ * Return: 1 if lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
