4a21f576817c5cbcceb5887379125cd7a88e98e0
sunnexy <sogbonnapaschal@gmail.com>
2022-03-24 08:21:19 -0700

    pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,18 +10,18 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
 
-	while (*(dest + count) != '\0'
+	while (*(dest + count) != '\0')
 	{
-	count++;
+		count++;
 	}
 
 	while (count2 < n)
 	{
-	*(dest + count) = *(src + count2);
-	if (*(src + count2) == '\0')
-	break;
-	count++;
-	count2++;
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
 	}
 	return (dest);
 }
