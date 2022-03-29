025264465f143d13ec5c23a4041b0359fba08978
sunnexy <sogbonnapaschal@gmail.com>
2022-03-29 08:30:05 -0700

    more

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _memset - ironnou
+ * _memset - fills memory with a constant byte
  * @s: pointer to char params
  * @b: data to change
  * @n: index
  * Return: *s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -16,5 +15,6 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
+
 	return (s);
 }
